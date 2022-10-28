a3946d4fa02f17c36c8cfda922b4edb8a2356ad2
Kim Seon Woo <seonwoo960000@naver.com>
2022-10-29 08:51:02 +0900

    Initial commit

---
+++ src/main/java/com/example/springbootmysqlqueryexample/SpringBootMysqlQueryExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootmysqlqueryexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMysqlQueryExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMysqlQueryExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootmysqlqueryexample/SpringBootMysqlQueryExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootmysqlqueryexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMysqlQueryExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
